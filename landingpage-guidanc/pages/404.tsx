cd1c0091b2a5bddbe7e529522769cc110af5e9a7
Lucas Sales <lucas.salles.oliveira@gmail.com>
2023-01-16 12:41:00 -0300

    criação do projeto next com typescript e estruturação básica do projeto

---
+++ landingpage-guidanc/pages/404.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+
+const Error404: NextPage = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Page not found</p>
+    </div>
+  )
+}
+
+export default Error404;
---
+++ landingpage-guidanc/pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from 'next/app'
+import "../styles/globals.scss"
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
