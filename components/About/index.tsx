0292d4205b3604530f254f7e97b24d81b5efbcff
Lucas Sales <lucas.salles.oliveira@gmail.com>
2023-01-17 01:39:20 -0300

    FRONT-7 MenuMobile Fixed
    Irregular padding and margin was fixed on MenuMobile in Navbar

---
+++ components/About/index.tsx
@@ -0,0 +1,11 @@
+import styles from './about.module.scss'
+
+const About: React.FC = () => {
+  return (
+    <section className={styles.container}>
+
+    </section>
+  )
+}
+
+export default About;
--- components/MainContainer/index.tsx
+++ components/MainContainer/index.tsx
@@ -8,7 +8,7 @@ const MainContainer: React.FC = () => {
         <h5>BOAS-VINDAS A GUIDANC 📚</h5>
         <h1>Mentoria para DEVs. Um jeito novo de entrar no mercado!</h1>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-        <button>Garanta sua vaga</button>
+        <button>Garanta sua vaga<i className="fa-solid fa-arrow-right"></i></button>
       </div>
       <div className={styles.image}>
         <img src={FlatImage.src} alt="" />
