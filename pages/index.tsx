1c45a18079fad3673cf81a8e025bf024ca421511
Lucas Sales <lucas.salles.oliveira@gmail.com>
2023-01-20 19:22:48 -0300

    FRONT-7 Favicon
    Add Favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,10 @@ const Home: React.FC = () => {
         <link rel="icon" href="/favicon.ico" />
         {/* WRONG WAY TO GET FONT-AWESOME FOR REACT */}
         <link rel="stylesheet" href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css' />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
 
       <Navbar title='Guidanc' />
