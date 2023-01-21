9e87f49f26e758244aa48b18644a86ab2561550a
Lucas Sales <lucas.salles.oliveira@gmail.com>
2023-01-21 17:45:00 -0300

    FRONT-7 Fontawesome installation
    The package was installed correctly with npm

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,8 +20,6 @@ const Home: React.FC = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="keywords" content="GuidanC devs, GuidanC mentoria, GuidanC mentoria de desenvolvedores, GuidanC aula, GuidanC aula dev jr" />
         <link rel="icon" href="/favicon.ico" />
-        {/* WRONG WAY TO GET FONT-AWESOME FOR REACT */}
-        <link rel="stylesheet" href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css' />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
