54f9475d870bdfc4941f91333a0dca722cb1d14c
Lucas Sales <lucas.salles.oliveira@gmail.com>
2023-01-26 15:18:08 -0300

    FRONT-7 Text Changed on Catch
    text content was changed on Catch

--- components/Catch/index.tsx
+++ components/Catch/index.tsx
@@ -5,8 +5,12 @@ const Catch: React.FC = () => {
   return (
     <section className={styles.container}>
       <div data-aos="fade-up" className={styles.box}>
-        <h1>Somos a primeira plataforma do Brasil focada em mentoria para desenvolvedores!</h1>
-        <p>Uma plataforma onde você irá aprender a como se tornar um desenvolvedor, e para sua primeira experiência, desenvolver projetos reais com a orientação de mentores, simulando um ambiente real de trabalho.</p>
+        <h1>Sua primeira experiência como dev!</h1>
+        <p>Uma plataforma onde você irá aprender a como se tornar dev. Para sua primeira experiência, você aprenderá a desenvolver projetos reais com a orientação de mentores, simulando um ambiente real de trabalho.</p>
+      </div>
+      <div data-aos="fade-up" className={styles.box}>
+        <h1>Desenvolva-se!</h1>
+        <p>Você também pode fazer nossas mentorias para subir de nível em sua carreira. Tendo uma boa visão do que é necessário para se desenvolver na área em que você deseja. </p>
       </div>
     </section>
   )
